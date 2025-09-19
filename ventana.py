8cbb8005af38b7cc066ff984d6d083396e20583f
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-09-19 15:51:03 -0600

    feat: Nuevo led que indica conexion

--- ventana.py
+++ ventana.py
@@ -21,6 +21,24 @@ def activar_rutina():
     else:
         print("El usuario eligió No")
 
+# Funcion para simular conexion o desconexion presionando led
+def toggle_led(event=None):
+    if label_led.cget("text") == "Desconectado":
+        canvas_led_conexion.itemconfig(led_conexion, fill="green")  # cambiar a verde
+        label_led.configure(text="Conectado")  # actualizar texto
+    else:  # si el LED está en verde (conectado)
+        canvas_led_conexion.itemconfig(led_conexion, fill="red")  # cambiar a rojo
+        label_led.configure(text="Desconectado")  # actualizar texto
+
+# Funcion para cambiar de modo oscuro a claro, o viceversa
+def cambiar_apariencia():
+    if switch_apariencia.get():
+        ctk.set_appearance_mode("dark")  # modo oscuro
+        canvas_led_conexion.config(bg=ventana.cget("bg"))
+    else:
+        ctk.set_appearance_mode("light")  # modo oscuro
+        canvas_led_conexion.config(bg=ventana.cget("bg"))
+
 # Click al boton de salir
 def activar_salir():
     """Cierra la ventana principal."""
@@ -30,13 +48,6 @@ def activar_salir():
         ventana.destroy()
     else:
         print("El usuario eligió No")
-
-# Funcion para cambiar de modo oscuro a claro, o viceversa
-def cambiar_apariencia():
-    if switch_apariencia.get():
-        ctk.set_appearance_mode("dark")  # modo oscuro
-    else:
-        ctk.set_appearance_mode("light")  # modo oscuro
 
 # Creacion de ventana principal, configurar tamano, titulo y color de fondo
 ventana = ctk.CTk()
@@ -48,12 +59,13 @@ ventana.grid_columnconfigure(0, weight=1) # Mantener centrados los elementos de 
 ventana.grid_rowconfigure(0, weight=0)  # fila del mensaje de bienvenida
 ventana.grid_rowconfigure(1, weight=0)  # fila del boton para manual
 ventana.grid_rowconfigure(2, weight=0)  # fila del boton para rutina
-ventana.grid_rowconfigure(3, weight=1)  # fila vacia para "empujar" el boton salir
-ventana.grid_rowconfigure(4, weight=0)  # fila del switch para modo claro u oscuro
+ventana.grid_rowconfigure(3, weight=0)  # fila vacia para "empujar" el boton salir
+ventana.grid_rowconfigure(4, weight=1)  # fila del switch para modo claro u oscuro
 ventana.grid_rowconfigure(5, weight=0)  # fila del boton para salir
+ventana.grid_rowconfigure(6, weight=0)  # fila del boton para salir
 
 # Etiqueta de bienvenida a la aplicacion
-etiqueta = ctk.CTkLabel(ventana, text="¡Bienvenido!\nControl de brazo robótico\nEstilo pinza", font=("Bebas Neue", 30))
+etiqueta = ctk.CTkLabel(ventana, text="¡Bienvenido!\nControl de brazo robótico\nEstilo pinza".lower(), font=("Bebas Neue", 30))
 etiqueta.grid(row=0, column=0, pady=30)
 
 # Boton para entrar en modo rutinario
@@ -74,19 +86,41 @@ boton_manual = ctk.CTkButton(ventana, text="Modo Manual 🎮",
                             command=activar_manual)
 boton_manual.grid(row=2, column=0, pady=10)
 
+# LED indicador de conexion con el robot
+# Frame contenedor del led y su texto
+frame_led = ctk.CTkFrame(ventana, fg_color="transparent")
+frame_led.grid(row=3, column=0, pady=10)
+
+# Canvas que contiene el led
+canvas_led_conexion = tk.Canvas(frame_led, 
+                                width=40, 
+                                height=40,
+                                highlightthickness=0, 
+                                bg=ventana.cget("bg"))
+canvas_led_conexion.grid(row=0, column=0, pady=10)
+canvas_led_conexion.bind("<Button-1>", toggle_led)
+
+# Dibujar circulo (Led apagado o rojo)
+led_conexion = canvas_led_conexion.create_oval(5,5,35,35,fill="red")
+
+# Label para el texto al lado del led
+label_led = ctk.CTkLabel(frame_led, 
+                        text="Desconectado", 
+                        font=("Bebas Neue", 30))
+label_led.grid(row=0, column=1, pady=10)
+
 # Switch para modo claro o modo oscuro del app
-switch_apariencia = ctk.CTkSwitch(ventana, text = "Modo Claro / Modo Oscuro", 
-                                command=cambiar_apariencia)
-switch_apariencia.grid(row=4, column=0, pady=25)
+switch_apariencia = ctk.CTkSwitch(ventana, text = "MODO CLARO / MODO OSCURO", command=cambiar_apariencia)
+switch_apariencia.grid(row=5, column=0, pady=15)
 
 # Boton para salir de la aplicacion
 boton_salir = ctk.CTkButton(ventana, text="Salir", 
-                            font=("Bebas Neue", 50), 
-                            width=200, 
-                            height=60,
+                            font=("Bebas Neue", 30), 
+                            width=70, 
+                            height=30,
                             corner_radius=13, 
                             command=activar_salir)
-boton_salir.grid(row=5, column=0, pady=25)
+boton_salir.grid(row=6, column=0, pady=15)
 
 # Loop para mantener ventana activa
 ventana.mainloop()
